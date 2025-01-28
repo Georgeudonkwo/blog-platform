1299753fb544392fc60ace6101f952c9d00c27d2
Georgeudonkwo <enogeorgeudonkwo@gmail.com>
2025-01-28 10:17:20 +0100

    user model added

---
+++ models/user.js
@@ -0,0 +1,19 @@
+import mongoose from 'mongoose';
+import bcrypt from 'bcryptjs';
+
+const userSchema = new mongoose.Schema({
+  username: { type: String, required: true, unique: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  followers: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
+  following: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
+});
+
+userSchema.pre('save', async function (next) {
+  if (this.isModified('password')) {
+    this.password = await bcrypt.hash(this.password, 12);
+  }
+  next();
+});
+
+export default mongoose.model('User', userSchema);
