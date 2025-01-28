29fa6b7475308b1012df0531e460e7f00d82c12d
Georgeudonkwo <enogeorgeudonkwo@gmail.com>
2025-01-28 16:33:00 +0100

    update to the route's documentation

--- routes/commentRoute.js
+++ routes/commentRoute.js
@@ -3,7 +3,31 @@ import { addComment } from '../controller/commentController.js';
 import authMiddleware from '../middleware/authMiddleware.js';
 
 const router = express.Router();
-
+/**
+ * @swagger
+ * /api/comments:
+ *   post:
+ *     summary: Create a new comment
+ *     tags: [comments]
+ *     security:
+ *       - bearerAuth: []
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               content:
+ *                 type: string
+ *     responses:
+ *       201:
+ *         description: comment created successfully
+ *       401:
+ *         description: Unauthorized
+ *       500:
+ *         description: Server error
+ */
 router.post('/comments', authMiddleware, addComment);
 
 export default router;
