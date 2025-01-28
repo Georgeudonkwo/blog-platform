95d2fff67409cc611c6d4882a8943ff1a0c099df
Georgeudonkwo <enogeorgeudonkwo@gmail.com>
2025-01-28 13:53:17 +0100

    modification made in the models module

--- main.js
+++ main.js
@@ -9,14 +9,8 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-
-// Connect to MongoDB
 connectDB();
-
-// body parser Middleware
 app.use(express.json());
-
-// Routes
 app.use('/api/auth', authRoutes);
 app.use('/api', postRoutes);
 app.use('/api', commentRoutes);
--- models/comment.js
+++ models/comment.js
@@ -1,10 +1,15 @@
 import mongoose from 'mongoose';
 
 const commentSchema = new mongoose.Schema({
-  content: { type: String, required: true },
-  author: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-  post: { type: mongoose.Schema.Types.ObjectId, ref: 'Post', required: true },
-  createdAt: { type: Date, default: Date.now },
+  content: { type: String,
+     required: true },
+  author: { type: mongoose.Schema.Types.ObjectId,
+     ref: 'user', 
+     required: true },
+  post: { type: mongoose.Schema.Types.ObjectId, 
+    ref: 'post', required: true },
+  createdAt: { type: Date,
+     default: Date.now },
 });
 
 export default mongoose.model('Comment', commentSchema);
