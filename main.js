e86447d51d6da98188714734174b8f28cc14bf69
Georgeudonkwo <enogeorgeudonkwo@gmail.com>
2025-01-30 04:39:45 +0100

    added downloadable pdf file for question 1 of the take home exams,in the public/docs directory

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 import express from 'express';
+import path from 'path';
+import { fileURLToPath } from "url";
 import connectDB from './dbconfig.js';
 import authRoutes from './routes/authRoutes.js';
 import postRoutes from './routes/postRoute.js';
@@ -6,6 +8,8 @@ import commentRoutes from './routes/commentRoute.js';
 import userRoute from './routes/userRoutes.js';
 import dotenv from 'dotenv';
 import swaggerSetup from './swagger.js';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 dotenv.config();
 
@@ -13,6 +17,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 connectDB();
 app.use(express.json());
+app.use("/docs/question1", express.static(path.join(__dirname, "public/docs")));
 app.use('/api/auth', authRoutes);
 app.use('/api', postRoutes);
 app.use('/api', commentRoutes);
